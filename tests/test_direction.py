19c011335da3645e3ce62e99b2a8293ff977964b
Andrew Hsieh <andrewhsieh1208@gmail.com>
2020-07-11 22:36:07 -0400

    removed pycache

--- tests/test_direction.py
+++ tests/test_direction.py
@@ -8,9 +8,25 @@ class DirectionTest(unittest.TestCase):
 
 	# test methods need to start with test_
 	def test_turn_right(self):
-		direction = Direction("N")
-		direction.turn_right()
-		self.assertEqual(direction.value, "E")
+		directionN = Direction("N")
+		directionN.turn_right()
+		self.assertEqual(directionN.value, "E")
+
+		directionE = Direction("E")
+		directionE.turn_right()
+		self.assertEqual(directionE.value, "S")
+
+		directionS = Direction("S")
+		directionS.turn_right()
+		self.assertEqual(directionS.value, "W")
+
+		directionW = Direction("W")
+		directionW.turn_right()
+		self.assertEqual(directionW.value, "N")
+
+
+
+
 
 if __name__ == "__main__":
 	unittest.main()
